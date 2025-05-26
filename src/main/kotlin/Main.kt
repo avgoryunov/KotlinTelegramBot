97670a1eeaea98c432b8495289c1e2df1573e24e
avgoryunov <av.goryunov58@gmail.com>
2025-05-27 00:16:49 +0300

    KTB-02-branch-name Создан файл для хранения текста "words.txt". В файле записаны слова на английском языке и переводы для них. С помощью метода "readLines()" и цикла "forEach{}" выполнена распечатка строчек из файла. Каждая строка из текстового файла выводится в консоль соответственно с новой строки.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package org.example
 
+import java.io.File
+
 fun main() {
 
+    val wordsFile = File("words.txt")
+    wordsFile.createNewFile()
+
+    wordsFile.writeText("hello привет\n")
+    wordsFile.appendText("dog собака\n")
+    wordsFile.appendText("cat кошка")
+
+    wordsFile.readLines().forEach { it -> println(it) }
 }
