744c7422ffa2e1ff8d6b2b7ae6018bb25276bdca
avgoryunov <av.goryunov58@gmail.com>
2025-05-28 23:59:20 +0300

    KTB-04-parse-the-dictionary-file Добавлено новое свойство в классе хранения слов. В файле "words" добавлен разделитель со значением равным 0, Для свойства "correctAnswersCount" добавлен элвис-оператор. Создан изменяемый список для словаря с типом объекта "Word". При создании объект с типом "Word" добавляется в словарь. Содержимое словаря выведено в консоль.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,32 @@ package org.example
 
 import java.io.File
 
-fun main() {
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
 
+fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
 
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка")
+    wordsFile.writeText("hello|привет|0")
+    wordsFile.appendText("\n")
+    wordsFile.appendText("dog|собака|0")
+    wordsFile.appendText("\n")
+    wordsFile.appendText("cat|кошка|0")
 
-    wordsFile.readLines().forEach { it -> println(it) }
+    val dictionary: MutableList<Word> = mutableListOf()
+    val lines: List<String> = wordsFile.readLines()
+
+    for (line in lines) {
+        val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
+        dictionary.add(word)
+    }
+
+    for (word in dictionary) {
+        println(word)
+    }
 }
