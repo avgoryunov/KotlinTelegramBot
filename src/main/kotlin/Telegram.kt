51cb3fa2e4db3f02b030d6bbcea502b92ab6b9f1
avgoryunov <av.goryunov58@gmail.com>
2025-06-17 22:43:51 +0300

    KTB-14-parse-a-string-using-regular-expressions-(RegEx) Выполнен парсинг отправляемого сообщения. Текст сообщения выведен в консоль. Парсинг update_id переписан с использованием Regex.

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -21,6 +21,13 @@ fun main(args: Array<String>) {
         if (startUpdateId == -1 || endUpdateId == -1) continue
         val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
         updateId = updateIdString.toInt() + 1
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+
+        println(text)
     }
 }
 
